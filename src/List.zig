7bdfc92f14082e24ff3df6e01845fe373aa03794
Bozhidar Stoyanov <bojidar.b.stoyanov@gmail.com>
2025-09-15 18:28:44 +0300

    chore: unify PyObject member names

--- src/List.zig
+++ src/List.zig
@@ -4,13 +4,13 @@ const convert = @import("convert.zig");
 
 const List = @This();
 
-object: *c.PyObject,
+py_object: *c.PyObject,
 
 pub const Iter = struct {
     iter: *c.PyObject,
 
     pub fn init(list: List) !Iter {
-        const maybe_iter: ?*c.PyObject = c.PyObject_GetIter(list.object);
+        const maybe_iter: ?*c.PyObject = c.PyObject_GetIter(list.py_object);
         return if (maybe_iter) |iter| .{ .iter = iter } else error.NotIterable;
     }
     pub fn deinit(iter: Iter) void {
@@ -36,21 +36,21 @@ pub const Iter = struct {
 };
 
 pub fn length(list: List) usize {
-    return @intCast(c.PyObject_Length(list.object));
+    return @intCast(c.PyObject_Length(list.py_object));
 }
 pub fn get(list: List, comptime T: type, index: usize) convert.ParseValueError!T {
     const py_value = list.getAny(index);
     return convert.pyObjectToValue(T, py_value);
 }
 pub fn sliceOfAny(list: List) [][*c]c.PyObject {
-    const py_list: *c.PyListObject = @ptrCast(list.object);
+    const py_list: *c.PyListObject = @ptrCast(list.py_object);
     const len: usize = list.length();
     return py_list.ob_item[0..len];
 }
 
 pub fn getAny(list: List, index: usize) *c.PyObject {
     @setRuntimeSafety(false); // no point in erroring on the @intCast here.
-    if (c.PyList_GetItem(list.object, @intCast(index))) |obj| {
+    if (c.PyList_GetItem(list.py_object, @intCast(index))) |obj| {
         return obj;
     } else {
         // @panic("index out of bounds");
@@ -60,11 +60,15 @@ pub fn getAny(list: List, index: usize) *c.PyObject {
 pub fn setAny(list: List, index: usize, value: *c.PyObject) void {
     @setRuntimeSafety(false); // no point in erroring on the @intCast here.
 
-    if (c.PyList_SetItem(list.object, @intCast(index), value) == 0) {} else {
+    if (c.PyList_SetItem(list.py_object, @intCast(index), value) == 0) {} else {
         unreachable;
     }
 }
 pub fn set(list: List, index: usize, value: anytype) void {
     const py_value = convert.valueToPyObject(value);
     list.setAny(index, py_value);
+}
+
+pub fn deinit(self: @This()) void {
+    c.Py_DecRef(self.py_object);
 }
